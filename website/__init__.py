d32eea78c0f2d6c8706b3776a07555f66135db62
JoseAntonioVelasco <57660135+JoseAntonioVelasco@users.noreply.github.com>
2021-03-21 15:45:31 +0100

    html chat y con4

--- website/__init__.py
+++ website/__init__.py
@@ -14,6 +14,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+
     from .views import views
     from .auth import auth
 
@@ -26,6 +27,7 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
+    login_manager.login_message = "Inicia sesión para poder acceder a la página."
     login_manager.init_app(app)
 
     @login_manager.user_loader
