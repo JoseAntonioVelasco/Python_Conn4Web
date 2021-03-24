256ef6315327dbeae31ba60506ef9923b12e1d1f
JoseAntonioVelasco <57660135+JoseAntonioVelasco@users.noreply.github.com>
2021-03-24 22:47:05 +0100

    commit antes de rebase/merge

--- website/events.py
+++ website/events.py
@@ -5,16 +5,33 @@ from . import socketio
 
 events = Blueprint('events',__name__)
 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('received something: '+str(json))
-    join_room(66)
-    socketio.emit('my response', json, room=66)
+
 
 @socketio.on('join')
 def join(message, namespace='/match'):
     print('ha entrado al join')
     matchID = int(session.get('room'))
-    print(matchID)
     join_room(matchID)
     emit('status', {'msg': session.get('username') + ' ha entrado a la sala'}, room = matchID)
+
+@socketio.on('text')
+def text(message, namespace='/match'):
+    print('ha entrado en text')
+    matchID = int(session.get('room'))
+    emit('message',{'msg': session.get('username') + ' : ' + message['msg']}, room = matchID)
+
+@socketio.on('left')
+def left(message, namespace='/match'):
+    matchID = int(session.get('room'))
+    username = session.get('username')
+    leave_room(room)
+    session.clear()
+    emit('status',{'msg: username' + 'ha salido de la sala'}, room = matchID)
+
+
+#test event
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received something: '+str(json))
+    join_room(66)
+    socketio.emit('my response', json, room=66)
